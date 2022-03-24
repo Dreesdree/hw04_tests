a569044937557ea63fcf747af173bc8c90f24594
Денис Иванов <deni_95@list.ru>
2022-03-24 13:08:27 +0500

    Спринт 5

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -40,7 +40,8 @@ class PostModelTest(TestCase):
         for field, expected_value in field_verboses.items():
             with self.subTest(field=field):
                 self.assertEqual(
-                    post._meta.get_field(field).verbose_name, expected_value)
+                    post._meta.get_field(field).verbose_name, expected_value
+                )
 
     def test_help_text_post(self):
         """Проверяем help_text совпадает с ожидаемым в модели Post."""
@@ -53,48 +54,50 @@ class PostModelTest(TestCase):
         for field, expected_value in field_help_texts.items():
             with self.subTest(field=field):
                 self.assertEqual(
-                    post._meta.get_field(field).help_text, expected_value)
+                    post._meta.get_field(field).help_text, expected_value
+                )
 
-    class GroupModelTest(TestCase):
-        @classmethod
-        def setUpClass(cls):
-            super().setUpClass()
-            cls.group = Group.objects.create(
-                title='Тестовая группа',
-                slug='Тестовый слаг',
-                description='Тестовое описание',
-            )
+class GroupModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='Тестовый слаг',
+            description='Тестовое описание',
+        )
 
-        def test_models_have_correct_object_names(self):
-            """Проверяем, что у модели Group корректно работает __str__."""
-            group = self.group
-            expected_object_name = group.title
-            self.assertEqual(expected_object_name, str(group))
+    def test_models_have_correct_object_names(self):
+        """Проверяем, что у модели Group корректно работает __str__."""
+        group = self.group
+        expected_object_name = group.title
+        self.assertEqual(expected_object_name, str(group))
 
-        def test_verbose_name_group(self):
-            """Проверяем verbose_name совпадает с ожидаемым в модели Group."""
-            group = self.group
-            field_verboses = {
-                'title': 'Группа',
-                'slug': 'Уникальный ID',
-                'description': 'Описание группы',
-            }
-            for field, expected_value in field_verboses.items():
-                with self.subTest(field=field):
-                    self.assertEqual(
-                        group._meta.get_field(field).verbose_name,
-                        expected_value
-                    )
+    def test_verbose_name_group(self):
+        """Проверяем verbose_name совпадает с ожидаемым в модели Group."""
+        group = self.group
+        field_verboses = {
+            'title': 'Группа',
+            'slug': 'Уникальный ID',
+            'description': 'Описание группы',
+        }
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    group._meta.get_field(field).verbose_name,
+                    expected_value
+                )
 
-        def test_help_text_group(self):
-            """Проверяем help_text совпадает с ожидаемым в модели Group."""
-            group = self.group
-            field_help_texts = {
-                'title': 'Название группы',
-                'slug': 'ID группы',
-                'description': 'Подробнее',
-            }
-            for field, expected_value in field_help_texts.items():
-                with self.subTest(field=field):
-                    self.assertEqual(
-                        group._meta.get_field(field).help_text, expected_value)
+    def test_help_text_group(self):
+        """Проверяем help_text совпадает с ожидаемым в модели Group."""
+        group = self.group
+        field_help_texts = {
+            'title': 'Название группы',
+            'slug': 'ID группы',
+            'description': 'Подробнее',
+        }
+        for field, expected_value in field_help_texts.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    group._meta.get_field(field).help_text, expected_value
+                )
